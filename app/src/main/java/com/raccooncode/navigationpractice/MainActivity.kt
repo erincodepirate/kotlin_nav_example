63b4b187199ad201d2ddc8badd73915b95c0a676
Erin Clark <erincodepirate@gmail.com>
2023-07-19 17:38:36 -0700

    Add kotlin navigation example

---
+++ app/src/main/java/com/raccooncode/navigationpractice/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.raccooncode.navigationpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.raccooncode.navigationpractice.ui.theme.NavigationPracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationPracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavigationAppHost(navController = navController)
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NavigationAppHost(navController: NavHostController) {
+    NavHost(navController = navController, "home") {
+        composable("home") {HomeScreen(navController)}
+        composable("feed") {FeedScreen(navController)}
+        composable("profile") {ProfileScreen(navController)}
+    }
+}
